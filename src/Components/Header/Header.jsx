c12a372dfd8bd387371b02828e19415a78842227
Camilo <user@CAmilos-MacBook-Pro.local>
2024-09-26 18:14:08 -0700

    header done 3 sizes screen

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { TiThMenu } from "react-icons/ti";
+import { MdClose } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+
 function Header() {
 const navigate = useNavigate();
+const [isVisible, setIsVisible] =useState(false)
 
+
+ const showMenu = () =>{
+    setIsVisible(!isVisible);
+ }
+ const closeMenu = () => {
+    setIsVisible(false);
+  };
   return (<>
     <header className='header'>
         <div
          onClick={()=>navigate('/')}
          className='logo'>Camilo Jorquera</div>
    <nav className='main-nav'>
-    <ul className='nav-links-wrap'>
-            <NavLink className='link' to='About'>//About</NavLink>
-            <NavLink className='link' to='/Projects'>//Projects</NavLink>
-  
+
+    <ul className={`nav-links-wrap ${isVisible ? 'visible' : ''}`}>
+            <NavLink
+             className='link' to='About'
+             onClick={closeMenu} >
+                <span className='arrow-name'><MdArrowForwardIos /></span>
+                <span className='link__name'>About</span>
+                
+             </NavLink>
+            <NavLink 
+            className='link'
+            onClick={closeMenu}
+             to='/Projects'>
+                 <span className='arrow-name'><MdArrowForwardIos /></span>
+             <span className='link__name'>Projects</span>
+                </NavLink>
+
     </ul>
-    
+    <button className='header-btn' onClick={showMenu}>
+    <TiThMenu className={`menu-icon ${!isVisible ? 'visible' : ''}`} />
+    <MdClose className={`close-icon ${isVisible ? 'visible' : ''}`} />
+        </button>
    </nav>
     </header>
     </>)
