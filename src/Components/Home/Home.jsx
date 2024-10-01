a63c1fd9f2ed4c4e027307025c639b1819b4311f
Camilo <user@CAmilos-MacBook-Pro.local>
2024-10-01 14:28:44 -0700

    animation implemented

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.scss';
 import developer from '../../Assets/Images/IMG_4036-removebg-preview.png';
 import mountain from '../../Assets/Images/IMG_4034-removebg-preview.png';
-// import bike from '../../Assets/Images/IMG_4035-removebg-preview.png';
+
 
 function Home() {
+
   return (<>
   <div className='home-container'>
   
     <div className='home-camilo-container'>
-            <span className='home-camilo home-camilo__name'>C</span>
-            <span className='home-camilo home-camilo__name'>a</span>
-            <span className='home-camilo home-camilo__name'>m</span>
-            <span className='home-camilo home-camilo__name'>i</span>
-            <span className='home-camilo home-camilo__name'>l</span>
-            <span className='home-camilo home-camilo__name'>o</span>
+            <span className='home-camilo home-camilo__1'>C</span>
+            <span className='home-camilo home-camilo__2'>a</span>
+            <span className='home-camilo home-camilo__3'>m</span>
+            <span className='home-camilo home-camilo__4'>i</span>
+            <span className='home-camilo home-camilo__5'>l</span>
+            <span className='home-camilo home-camilo__6'>o</span>
       </div>
             <div className='home-camilo-last-name'>
-                    <span className='home-camilo home-camilo__last'>J</span>
-                    <span className='home-camilo home-camilo__last'>o</span>
-                    <span className='home-camilo home-camilo__last'>r</span>
-                    <span className='home-camilo home-camilo__last'>q</span>
-                    <span className='home-camilo home-camilo__last'>u</span>
-                    <span className='home-camilo home-camilo__last'>e</span>
-                    <span className='home-camilo home-camilo__last'>r</span>
-                    <span className='home-camilo home-camilo__last'>a</span>
+                    <span className='home-camilo  home-camilo__7'>J</span>
+                    <span className='home-camilo  home-camilo__8'>o</span>
+                    <span className='home-camilo  home-camilo__9'>r</span>
+                    <span className='home-camilo  home-camilo__10'>q</span>
+                    <span className='home-camilo  home-camilo__11'>u</span>
+                    <span className='home-camilo  home-camilo__12'>e</span>
+                    <span className='home-camilo  home-camilo__13'>r</span>
+                    <span className='home-camilo  home-camilo__14'>a</span>
             </div>
             <h2 className='home-subtitle'>SOFTWARE ENGINEER</h2>
-            <div className='home-images-container'>
+            <div 
+            className='home-images-container'>
                 <img  className='developer' src={developer}/>
                 <img  className='mountain' src={mountain}/>
-                {/* <img  className='bike' src={bike}/> */}
+
             </div>
 
     </div>
